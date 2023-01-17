98d9bcde9d2e09192a2872b9dab17626c9b5f0c4
Michael Jay <metr00@users.noreply.github.com>
2023-01-16 23:26:19 -0800

    Add files via upload

--- cryptoware2.py
+++ cryptoware2.py
@@ -6,6 +6,7 @@
 # 1-13-2023
 import os
 import time
+import binascii
 from cryptography.fernet import Fernet, InvalidToken, InvalidSignature
 import argparse
 
@@ -17,11 +18,12 @@ class ansi:
     CYAN = '\033[1;36m'
     END = '\033[0m'
 
+
 ##################################ENCRYPT FUNCTION###########################
 # directory function
-def encrypt_subfolders(key, directory):
+def encrypt_subfolders(key, dir_path):
     filecount = 0
-    for dirpath, dirnames, filenames in os.walk(directory):
+    for dirpath, dirnames, filenames in os.walk(dir_path):
         for filename in filenames:
             if filename.endswith('.crypt') or filename == os.path.basename(__file__):
                 continue
@@ -30,10 +32,12 @@ def encrypt_subfolders(key, directory):
             file_path = os.path.join(dirpath, filename)
             encrypt_file(key, file_path)
             os.rename(file_path, file_path + '.crypt')
-            print(f'[  {ansi.GREEN}OK{ansi.END}  ]{file_path} has been encrypted')	
+            #print(f'[  {ansi.GREEN}OK{ansi.END}  ]{file_path} has been encrypted')	
             filecount += 1
-    return filecount
-    
+    print('[  FINISH  ] Encryption')
+    print(f'\n{ansi.GREEN}[ {filecount} files has been encrypted ]{ansi.END}\n')
+
+
 # encryption function
 def encrypt_file(key, file_path):
     try:
@@ -42,23 +46,27 @@ def encrypt_file(key, file_path):
         encrypted_contents = Fernet(key).encrypt(contents)
         with open(file_path, 'wb') as f:
             f.write(encrypted_contents)
+        print(f'[  {ansi.GREEN}OK{ansi.END}  ] {file_path} has been encrypted')
+
     except (IOError, OSError):
         print(f'[  {ansi.RED}ERROR{ansi.END}  ] Unable to encrypt {file_path}')
 
 ############################DECRYPT FUNCTION#################################
 
-def decrypt_subfolders(key, directory):
+def decrypt_subfolders(key, dir_path):
     filecount = 0
-    for dirpath, dirnames, filenames in os.walk(directory):
+    for dirpath, dirnames, filenames in os.walk(dir_path):
         for filename in filenames:
             if not filename.endswith('.crypt'):
                 continue
             file_path = os.path.join(dirpath, filename)
             decrypt_file(key, file_path)
             os.rename(file_path, file_path[:-6])
-            print(f'[  {ansi.GREEN}OK{ansi.END}  ]{file_path} has been decrypted')
+            #print(f'[  {ansi.GREEN}OK{ansi.END}  ]{file_path} has been decrypted')
             filecount += 1
-    return filecount 
+    print('[  FINISH  ] Decryption')
+    print(f'\n{ansi.GREEN}[ {filecount} files has been decrypted ]{ansi.END}\n') 
+
 
 def decrypt_file(key, file_path):
     try:
@@ -67,12 +75,13 @@ def decrypt_file(key, file_path):
         decrypted_contents = Fernet(key).decrypt(contents)
         with open(file_path, 'wb') as f:
             f.write(decrypted_contents)
+            print(f'[  {ansi.GREEN}OK{ansi.END}  ] {file_path} has been decrypted')
+
     except (IOError, OSError):
         print(f'[  {ansi.RED}ERROR{ansi.END}  ] Unable to decrypt {file_path}')
 
 
 def main():
-
     parser = argparse.ArgumentParser('./cryptoware2.py [yourdirectory]')
     parser.add_argument('path', help='path to directory')
     parser.add_argument('-e', '--encrypt', action='store_true', help='encrypts the folder')
@@ -80,60 +89,38 @@ def main():
     parser.add_argument('-k', '--key', nargs='?', help='key text')
     parser.add_argument('-K', '--keyfile', nargs='?', help='key file')
     args = parser.parse_args()
-    directory = args.path
+    dir_path = args.path
 
+
+    # ---encrypt
     if args.encrypt:
         key = Fernet.generate_key()
-        
-        print('\n[  START  ] Encryption')
-        encrypted_files = encrypt_subfolders(key, directory)
-        print('[  FINISH  ] Encryption')
-        print(f'\n{ansi.RED}[ {encrypted_files} has been encrypted ]{ansi.END}\n')
+        encrypt_subfolders(key, dir_path)
         print('-' * 66)
         print(f'[*] this is your key: {ansi.CYAN}{str(key)[2:-1]}{ansi.END}')
         print('-' * 66)
+        exit() 
 
-    #TODO fix this logic
-    elif args.decrypt and not args.key:
+    # --decrypt
+    if args.decrypt:
 
-        if args.decrypt and args.keyfile:
-            try:
-                keyfile = args.keyfile
-                with open(keyfile, 'rb') as f:
-                    key = f.read()
-                print('[  START  ] Decryption')
-                decrypted_files = decrypt_subfolders(key, directory)
-                print('[  FINISH  ] Decryption')
-                print(f'\n{ansi.RED}[ {decrypted_files} has been decrypted ]{ansi.END}\n')
-
-            except (InvalidToken, InvalidSignature):
-                print('\n[!] this is the wrong key')
-
-            except FileNotFoundError:
-                print('\n[!] file not found')
- 
-        else: 
-            print('you forgot to enter the key')
-            print('./cryptoware2.py [yourdirectory] -d -k [yourkey]')
-            print('./cryptoware2.py [yourdirectory] -d -K [yourkeyfile]') 
-
-    elif args.decrypt and args.key:
         try:
-            key = bytes(args.key, 'utf-8')
-            print('[  START  ] Decryption')
-            decrypted_files = decrypt_subfolders(key, directory)
-            print('[  FINISH  ] Decryption')
-            print(f'\n{ansi.RED}[ {decrypted_files} has been decrypted ]{ansi.END}\n')
-
-        except (InvalidToken, InvalidSignature):
-            print('\n[!] this is the wrong key')
+            if args.key:  #  --key
+                key = bytes(args.key, 'utf-8')
+            elif args.keyfile:  # if --Keyfile
+                with open(args.keyfile, 'rb') as f:
+                    key = f.read()
+            else:
+                raise ValueError("No key or keyfile provided.")
+            decrypt_subfolders(key, dir_path)
+        except (InvalidToken, InvalidSignature, binascii.Error, ValueError) as e:
+            print(f'\n[!] {e}')
+        except FileNotFoundError as e:
+            print(f'\n[!] {e}')
 
     else:
-        print('error no arguments')
-        print('usage: ./cryptoware2.py [yourdirectory] --encrypt --decrypt --key [yourkey] --keyfile [yourkeyfile]')
+        parser.print_help() 
+
 
 if __name__ == "__main__":
-    main()
-    
-
-    
+    main()   
